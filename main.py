ff1f9a8ae2660cb593e330cd4cae104284039252
Omar Barazanji <ozanji345@gmail.com>
2023-03-07 19:28:22 -0600

    latest

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import gevent.subprocess as subprocess
+import sys
+args = sys.argv
+
+MODE = 'dev'
+if len(args)>1: MODE = args[1]
 
 def start_server():
         '''
         Boots the NLP Server for API calls.
         '''
         print('\n[Starting NLP Server...]')
-        subprocess.call(['python', 'start_server.py'])
+        if MODE=='dev': subprocess.call(['python', 'start_server.py'])
+        else: subprocess.call(['python', '~/nlp_server/start_server.py'])
         
 
 if __name__ == "__main__":
