6324e994bff565e8dae334eef388858afaa3f2d8
Omar Barazanji <ozanji345@gmail.com>
2023-03-07 19:29:26 -0600

    latest

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def start_server():
         Boots the NLP Server for API calls.
         '''
         print('\n[Starting NLP Server...]')
-        if MODE=='dev': subprocess.call(['python', 'start_server.py'])
-        else: subprocess.call(['python', '~/nlp_server/start_server.py'])
+        if MODE=='prod': subprocess.call(['python', '~/nlp_server/start_server.py'])
+        else: subprocess.call(['python', 'start_server.py'])
         
 
 if __name__ == "__main__":
