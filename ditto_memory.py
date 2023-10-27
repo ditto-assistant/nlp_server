cd25ed2c96ead9e0358b4f5e6ab7c6652ef6338c
omarzanji <omarzanji@users.noreply.github.com>
2023-10-27 01:47:04 +0000

    :art: Format Python code with psf/black

--- ditto_memory.py
+++ ditto_memory.py
@@ -17,6 +17,7 @@ import logging
 
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # import google search agent
@@ -34,6 +35,7 @@ logging.basicConfig(level=logging.INFO)
 from templates.llm_tools import LLM_TOOLS_TEMPLATE
 from templates.default import DEFAULT_TEMPLATE
 
+
 class DittoMemory:
     def __init__(self, verbose=False):
         self.verbose = verbose
@@ -45,17 +47,22 @@ class DittoMemory:
 
     def __handle_params(self):
         self.llm_provider = os.environ["LLM"]
-        self.template_type = "DEFAULT" if "SERPER_API_KEY" not in os.environ else "SELF-ASK-WITH-SEARCH"
-        self.template = DEFAULT_TEMPLATE if "SERPER_API_KEY" not in os.environ else LLM_TOOLS_TEMPLATE
+        self.template_type = (
+            "DEFAULT" if "SERPER_API_KEY" not in os.environ else "SELF-ASK-WITH-SEARCH"
+        )
+        self.template = (
+            DEFAULT_TEMPLATE
+            if "SERPER_API_KEY" not in os.environ
+            else LLM_TOOLS_TEMPLATE
+        )
         if self.llm_provider == "huggingface":
             # repo_id = "google/flan-t5-xxl"
             repo_id = "codellama/CodeLlama-13b-hf"
             self.llm = HuggingFaceHub(
                 repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 3000}
             )
-        else: # default to openai
+        else:  # default to openai
             self.llm = ChatOpenAI(temperature=0.4, model_name="gpt-3.5-turbo-16k")
-            
 
     def __create_load_memory(self, reset=False, user_id="Human"):
         mem_dir = f"memory/{user_id}"
@@ -81,7 +88,9 @@ class DittoMemory:
                 {"Ditto": "Hi! My name is Ditto. Nice to meet you!"},
             )
             self.memory[user_id].save_context(
-                {f"{user_id}": "Hey Ditto! Nice to meet you too. Glad we can be talking."},
+                {
+                    f"{user_id}": "Hey Ditto! Nice to meet you too. Glad we can be talking."
+                },
                 {"Ditto": "Me too!"},
             )
             pickle.dump(self.memory[user_id], open(mem_file, "wb"))
@@ -103,46 +112,65 @@ class DittoMemory:
         log.info(f"Reset memory for {user_id}")
 
     def add_example_to_query(self, query, stmem_query, examples):
-        query_prefix = "\n(You do not need to use these past memories if not relevant)" \
-            + "\n\nTools:\n" \
-            + "Ditto has access to the following tools, and they can be used in the following ways:\n" \
-            + "1. GOOGLE_SEARCH: <GOOGLE_SEARCH> <query>\n" \
-            + "1.a GOOGLE_SEARCH can be used to search the web for information. Only use this tool if the user's prompt can be better answered by searching the web."\
+        query_prefix = (
+            "\n(You do not need to use these past memories if not relevant)"
+            + "\n\nTools:\n"
+            + "Ditto has access to the following tools, and they can be used in the following ways:\n"
+            + "1. GOOGLE_SEARCH: <GOOGLE_SEARCH> <query>\n"
+            + "1.a GOOGLE_SEARCH can be used to search the web for information. Only use this tool if the user's prompt can be better answered by searching the web."
             + "\n\nIf the user's prompt can be answered by one of these tools, Ditto will use it to answer the question. Otherwise, Ditto will answer the question itself.\n\n"
+        )
         query = query_prefix + examples + "\n" + stmem_query
         return query
-    
-    def prompt(self, query, user_id="Human"):
 
+    def prompt(self, query, user_id="Human"):
         self.__create_load_memory(user_id=user_id)
         stamp = str(datetime.utcfromtimestamp(time.time()))
 
         mem_query = f"Timestamp: {stamp}\n{query}"
 
-        if self.template_type == 'DEFAULT':
-            prompt = PromptTemplate(input_variables=["history", "input"], template=self.template)
-            stmem_query = self.short_term_mem_store.get_prompt_with_stmem(user_id, query)
+        if self.template_type == "DEFAULT":
+            prompt = PromptTemplate(
+                input_variables=["history", "input"], template=self.template
+            )
+            stmem_query = self.short_term_mem_store.get_prompt_with_stmem(
+                user_id, query
+            )
             conversation_with_memory = ConversationChain(
-                llm=self.llm, prompt=prompt, memory=self.memory[user_id], verbose=self.verbose
+                llm=self.llm,
+                prompt=prompt,
+                memory=self.memory[user_id],
+                verbose=self.verbose,
             )
             res = conversation_with_memory.predict(input=stmem_query)
         else:
-            stmem_query = self.short_term_mem_store.get_prompt_with_stmem(query, user_id)
+            stmem_query = self.short_term_mem_store.get_prompt_with_stmem(
+                query, user_id
+            )
             examples = self.example_store.get_example(query)
-            query_with_examples = self.add_example_to_query(query, stmem_query, examples)
-            prompt = PromptTemplate(input_variables=["history", "input"], template=self.template, memory=self.memory[user_id])
+            query_with_examples = self.add_example_to_query(
+                query, stmem_query, examples
+            )
+            prompt = PromptTemplate(
+                input_variables=["history", "input"],
+                template=self.template,
+                memory=self.memory[user_id],
+            )
             conversation_with_memory = ConversationChain(
-                llm=self.llm, prompt=prompt, memory=self.memory[user_id], verbose=self.verbose
+                llm=self.llm,
+                prompt=prompt,
+                memory=self.memory[user_id],
+                verbose=self.verbose,
             )
             res = conversation_with_memory.predict(input=query_with_examples)
 
         if "GOOGLE_SEARCH" in res:
             log.info(f"Handling prompt for {user_id} with Google Search Agent")
-            ditto_command = '<GOOGLE_SEARCH>'
+            ditto_command = "<GOOGLE_SEARCH>"
             ditto_query = res.split("GOOGLE_SEARCH")[-1].strip()
             res = self.google_search_agent.handle_google_search(res)
-            res = res + '\n-LLM Tools: Google Search-' 
-            memory_res = f'{ditto_command} {ditto_query} \nGoogle Search Agent: ' + res
+            res = res + "\n-LLM Tools: Google Search-"
+            memory_res = f"{ditto_command} {ditto_query} \nGoogle Search Agent: " + res
         else:
             memory_res = res
 
@@ -153,6 +181,4 @@ class DittoMemory:
 
 
 if __name__ == "__main__":
-    ditto = DittoMemory(
-        verbose=True
-    )
+    ditto = DittoMemory(verbose=True)
--- fallback_agent.py
+++ fallback_agent.py
@@ -1,6 +1,6 @@
-'''
+"""
 This is an LLM agent used to handle GOOGLE_SEARCH commands from Ditto Memory agent.
-'''
+"""
 
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
@@ -18,45 +18,52 @@ logging.basicConfig(level=logging.INFO)
 
 # load env
 from dotenv import load_dotenv
+
 load_dotenv()
 
 LLM = os.environ.get("LLM")
 
-class GoogleSearchFallbackAgent():
 
+class GoogleSearchFallbackAgent:
     def __init__(self, verbose=False):
         self.initialize_agent(verbose)
-    
+
     def initialize_agent(self, verbose):
-        '''
+        """
         This function initializes the agent.
-        '''
+        """
         if LLM == "openai":
             llm = ChatOpenAI(temperature=0.4, model_name="gpt-3.5-turbo")
         else:
             repo_id = "codellama/CodeLlama-13b-hf"
-            llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 3000})
-        
+            llm = HuggingFaceHub(
+                repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 3000}
+            )
+
         fallback_agent_tools = load_tools(["google-serper"], llm=llm)
         self.search = GoogleSerperAPIWrapper()
         self.fallback_agent = initialize_agent(
-            fallback_agent_tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION , verbose=verbose)
-        
+            fallback_agent_tools,
+            llm,
+            agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            verbose=verbose,
+        )
 
     def handle_google_search(self, query):
-        '''
+        """
         This function handles GOOGLE_SEARCH commands from Ditto Memory agent.
-        '''
+        """
         try:
             response = self.fallback_agent.run(query)
         except Exception as e:
             log.info(f"Error running fallback agent: {e}")
             response = f"Error running google search agent: {e}"
-            if 'LLM output' in response:
-                response = response.split('`')[1]
-            
+            if "LLM output" in response:
+                response = response.split("`")[1]
+
         return response
-    
+
+
 if __name__ == "__main__":
     google_search_agent = GoogleSearchFallbackAgent(verbose=True)
     query = "What is steeve job's birthday?"
